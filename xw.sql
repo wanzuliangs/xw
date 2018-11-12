1b5bf4c5472095f8a1d3351f0348c149233608ad
wan <704412139@qq.com>
2018-11-12 17:57:29 +0800

    数据库文件备份

---
+++ xw.sql
@@ -0,0 +1,41 @@
+/*
+SQLyog Ultimate v11.11 (64 bit)
+MySQL - 5.5.53 : Database - xw
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`xw` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `xw`;
+
+/*Table structure for table `x_admin` */
+
+DROP TABLE IF EXISTS `x_admin`;
+
+CREATE TABLE `x_admin` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '管理员id',
+  `account` varchar(50) NOT NULL DEFAULT '' COMMENT '管理员帐号',
+  `password` varchar(32) NOT NULL DEFAULT '' COMMENT '管理员密码',
+  `state` tinyint(1) DEFAULT '1' COMMENT '状态 0,1 (1为正常,1为锁定)',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `account` (`account`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COMMENT='管理员表';
+
+/*Data for the table `x_admin` */
+
+LOCK TABLES `x_admin` WRITE;
+
+UNLOCK TABLES;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
