0011964ee09e0550fb2790a6a5ef26c5cfaaba70
wan <704412139@qq.com>
2018-11-15 17:44:41 +0800

    登陆日志模块

--- application/admin/controller/Login.php
+++ application/admin/controller/Login.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace app\admin\controller;
 
 use think\Controller;
@@ -9,28 +10,54 @@ class Login extends Controller
     {
         if (request()->isPost()) {
             $data = input('post.');
-            $validate = validate('Manager');
-            $result = $validate->scene('login')->check($data);
-            if (!$result) {
-                $this->error($validate->getError());
+            $res = $this->checkLogin($data);
+//            if (isset($res['aid'])) {
+//                $logInfo = array(
+//                    'uid' => $res['aid'],
+//                    'ip' => request()->ip(),
+//                    'logintime' => time(),
+//                    'msg' => $res['msg'],
+//                );
+//                db('log')->insert($logInfo);
+//            }
+            if ($res['code'] == 1) {
+                $this->success($res['msg'], 'index/index');
             }
-            $info = db('admin')->where('account',$data['account'])->find();
-            if (!$info) {
-                $this->error('帐号不存在!');
-            }
-            if ($info['password'] != md5($data['password'])) {
-                $this->error('密码不正确!');
-            }
-            session('loginname',$data['account'],'admin');
-            session('loginid',$info['id'],'admin');
-            $this->success('登陆成功!','index/index');
+            $this->error($res['msg']);
         }
         // 解决重复登陆问题
-        if (session('loginname','','admin') && session('loginid','','admin'))
-        {
+        if (session('loginname', '', 'admin') && session('loginid', '', 'admin')) {
             $this->redirect('index/index');
         }
         return view();
+    }
+
+    public function checkLogin($data)
+    {
+        $validate = validate('Manager');
+        $result = $validate->scene('login')->check($data);
+        if (!$result) {
+//            $this->error($validate->getError());
+            return ['code' => 0, 'msg' => $validate->getError()];
+        }
+        $info = db('admin')->where('account', $data['account'])->find();
+
+        if (!$info) {
+//            $this->error('帐号不存在!');
+            return ['code' => 0, 'msg' => '帐号不存在'];
+        }
+        if ($info['password'] != md5($data['password'])) {
+//            $this->error('密码不正确!');
+            return ['code' => 0, 'msg' => '密码不正确', 'aid' => $info['id']];
+        }
+        if ($info['state'] == 0) {
+            return ['code' => 0, 'msg' => '用户已被锁定', 'aid' => $info['id']];
+        }
+
+        session('loginname', $data['account'], 'admin');
+        session('loginid', $info['id'], 'admin');
+//        $this->success('登陆成功!','index/index');
+        return ['code' => 1, 'msg' => '登陆成功', 'aid' => $info['id']];
     }
 
     /**
@@ -38,7 +65,7 @@ class Login extends Controller
      */
     public function logout()
     {
-        session(null,'admin');
+        session(null, 'admin');
         $this->redirect('index');
     }
 }
--- xw.sql
+++ xw.sql
@@ -27,11 +27,33 @@ CREATE TABLE `x_admin` (
   `state` tinyint(1) DEFAULT '1' COMMENT '状态 0,1 (1为正常,1为锁定)',
   PRIMARY KEY (`id`),
   UNIQUE KEY `account` (`account`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COMMENT='管理员表';
+) ENGINE=MyISAM AUTO_INCREMENT=17 DEFAULT CHARSET=utf8 COMMENT='管理员表';
 
 /*Data for the table `x_admin` */
 
 LOCK TABLES `x_admin` WRITE;
+
+insert  into `x_admin`(`id`,`account`,`password`,`state`) values (1,'admin','40be4e59b9a2a2b5dffb918c0e86b3d7',1),(3,'admins-pc','40be4e59b9a2a2b5dffb918c0e86b3d7',1),(9,'aaaa','40be4e59b9a2a2b5dffb918c0e86b3d7',0),(10,'bbbb','40be4e59b9a2a2b5dffb918c0e86b3d7',1),(14,'sgsgsgsg','40be4e59b9a2a2b5dffb918c0e86b3d7',1),(15,'java','40be4e59b9a2a2b5dffb918c0e86b3d7',1),(16,'kekaka','40be4e59b9a2a2b5dffb918c0e86b3d7',1);
+
+UNLOCK TABLES;
+
+/*Table structure for table `x_log` */
+
+DROP TABLE IF EXISTS `x_log`;
+
+CREATE TABLE `x_log` (
+  `uid` int(11) NOT NULL DEFAULT '0' COMMENT '登陆id',
+  `ip` char(15) DEFAULT '' COMMENT '登陆ip',
+  `logintime` int(10) DEFAULT '0' COMMENT '登陆时间',
+  `msg` varchar(80) DEFAULT '' COMMENT '登陆信息',
+  PRIMARY KEY (`uid`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COMMENT='登陆日志表';
+
+/*Data for the table `x_log` */
+
+LOCK TABLES `x_log` WRITE;
+
+insert  into `x_log`(`uid`,`ip`,`logintime`,`msg`) values (1,'127.0.0.1',1542271493,'登陆成功');
 
 UNLOCK TABLES;
 
