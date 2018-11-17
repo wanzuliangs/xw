6b47c7608d8d733982cb0c10a17890f9e5cbea49
wan <704412139@qq.com>
2018-11-17 14:31:18 +0800

    完成站点配置模块上传logo功能

--- application/admin/controller/Config.php
+++ application/admin/controller/Config.php
@@ -1,5 +1,7 @@
 <?php
+
 namespace app\admin\controller;
+
 use think\Controller;
 
 class Config extends Controller
@@ -8,18 +10,32 @@ class Config extends Controller
     {
         if (request()->isPost()) {
             $data = input('post.');
+            // 获取表单传过来的图片
+            $file = input('file.imglogo');
+            if ($file) {
+                $data['img'] = $this->upload($file);
+            }
             $data_config = json_encode($data, JSON_UNESCAPED_UNICODE);
             $configInfo = db('config')->find();
             if (!$configInfo) {
                 db('config')->insert(['cfg' => $data_config]);
             } else {
-                db('config')->where('id',$configInfo['id'])->update(['cfg'=>$data_config]);
+                db('config')->where('id', $configInfo['id'])->update(['cfg' => $data_config]);
             }
             $this->success('修改配置成功!');
         }
         $config = db('config')->find();
-        $configInfo = json_decode($config['cfg'],true);
-        $this->assign('configInfo',$configInfo);
+        $configInfo = json_decode($config['cfg'], true);
+        $this->assign('configInfo', $configInfo);
         return view();
+    }
+
+    protected function upload($file)
+    {
+        if ($file) {
+            $info = $file->move(ROOT_PATH . 'public' . DS . 'uploads','logo');
+            $filename = $info->getFilename();
+            return $filename;
+        }
     }
 }
