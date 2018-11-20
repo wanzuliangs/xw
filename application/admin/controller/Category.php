b2e557a59654c5516f2d1addbe953545c8ceaa2b
wan <704412139@qq.com>
2018-11-20 09:51:49 +0800

    完成栏目分类的添加

--- application/admin/controller/Category.php
+++ application/admin/controller/Category.php
@@ -15,10 +15,14 @@ class Category extends Controller
             if (!$validate->check($data)) {
                 $this->error($validate->getError());
             }
-            CateModel::create($data);
-
+            if (CateModel::create($data)) {
+                $this->success('栏目分类添加成功!');
+            }
 
         }
+        $list = CateModel::all();
+        $res = CateModel::getCate($list);
+        $this->assign('catelist',$res);
         return view();
     }
 }
--- application/admin/model/Category.php
+++ application/admin/model/Category.php
@@ -6,5 +6,25 @@ use think\Model;
 
 class Category extends Model
 {
-
+    // 获取栏目分类
+    public static function getCate($list,$pid = 0,$lev=-1)
+    {
+        static $arr = array();
+        $lev+=1;
+        if ($lev == 0) {
+            $str = '';
+        } else {
+            $str = '|';
+        }
+        foreach ($list as $v) {
+            if ($v['pid'] == $pid) {
+                $tmp_arr['id'] = $v['id'];
+                $tmp_arr['name'] = $str . str_repeat('---',$lev) . $v['name'];
+                $tmp_arr['pid'] = $v['pid'];
+                $arr[] = $tmp_arr;
+                self::getCate($list,$v['id'],$lev);
+            }
+        }
+        return $arr;
+    }
 }
